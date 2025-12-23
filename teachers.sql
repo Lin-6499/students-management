a7b167788a65645c5ccf2c7858d3f10ac956fe88
Lin-6499 <lin_6499@qq.com>
2025-12-23 10:58:27 +0800

    整理文件，将前端放入frontend文件夹

--- teachers.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2001, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2002, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2003, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2004, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2005, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2006, '应用技术学院', '讲师');
-INSERT INTO `teachers`(`user_id`, `department`, `title`) VALUES (2007, '应用技术学院', '讲师');
